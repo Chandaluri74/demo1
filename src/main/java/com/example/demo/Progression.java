5fce1ed9aaf5e902a9be44dddbe51f4bdc96d531
Ganesh <chandaluri74@gmail.com>
2025-04-20 11:12:40 -0500

    this is third commit

--- src/main/java/com/example/demo/Progression.java
+++ src/main/java/com/example/demo/Progression.java
@@ -3,4 +3,13 @@ package com.example.demo;
 public class Progression {
     public int first;
     public int second;
+
+    public Progression(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public int addTwoNumbers(int i, int j){
+        return i+j;
+    }
 }
